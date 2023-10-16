e3a587f57003aded6f659e3c7b48db8794b38a19
Orengia Christian <orengiachristian@gmail.com>
2023-10-16 10:59:14 -0300

    kickoff

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,12 +7,12 @@ function Footer() {
         <div id="footer" className="bg-gray-200 absolute bottom-0  w-full">
             <div className="textoDorado inset-y-0  grid grid-cols-3 align-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div className="flex p-1 align-center my-auto">
-                    <a href="mailto:piattipastas@gmail.com" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FiMail/></a>
+                    <a href="mailto:maripastas@gmail.com" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FiMail/></a>
                     <a href="https://api.whatsapp.com/send?phone=5493467499337" className="mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl"><FaWhatsapp className='' /></a>
-                    <a href="https://www.instagram.com/piattipastas/" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FaInstagram/></a>
-                    <a href="https://www.facebook.com/piattipastas/" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FaFacebook/></a>
+                    <a href="https://www.instagram.com/maripastas/" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FaInstagram/></a>
+                    <a href="https://www.facebook.com/maripastas/" className='mx-2 xl:mx-4 text-lg hover:text-stone-600 focus:outline-none xl:text-2xl'><FaFacebook/></a>
                 </div>
-                <h1 className="logoPiatti text-4xl xl:text-6xl text-slate-600">Piatti</h1>
+                <h1 className="logomari text-4xl xl:text-6xl text-slate-600">Mari</h1>
                 <div className="my-auto">
                     <ul className="text-xs xl:text-sm text-right mr-2 xl:mr-4 ">
                         <li>Blvd. San Martín 53</li>
@@ -21,7 +21,7 @@ function Footer() {
                     </ul>                    
                 </div>
             </div>
-            <p className="fondoDorado text-xs xl:text-md  text-white">Copyright 2020 Piatti pastas. Todos los derechos reservados</p>
+            <p className="fondoDorado text-xs xl:text-md  text-white">Copyright 2020 Mari pastas. Todos los derechos reservados</p>
             <WhatsappWidget/>
         </div>
     )
